945ff74b480be7f04df0448a82a407462fe459a1
Muhammad Burhan <muhammadburhan@student.telkomuniversity.ac.id>
2023-03-20 07:51:32 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using modul5_1302213109;
+
+long angka1 = new Console.ReadLine();
+long angka2 = new Console.ReadLine();
+long angka3 = new Console.ReadLine();
+
+Penjumlahan<long> ab = new Penjumlahan<long>(angka1, angka2, angka3);
