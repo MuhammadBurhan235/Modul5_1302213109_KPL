4798dd92cebad942365c52b96c44c91b654b3c32
Muhammad Burhan <muhammadburhan@student.telkomuniversity.ac.id>
2023-03-20 08:17:36 +0700

    THIRD COMMIT

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using modul5_1302213109;
+
+String data = new Console.ReadLine();
+String tanggal = new Console.ReadLine();
+SimpleDataBase<String, String> bz = new SimpleDataBase<String, String>(data, tanggal);
---
+++ SimpleDataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213109
+{
+    internal class SimpleDataBase <T, Date>
+    {
+        private List<T> storedData;
+        private List<Date> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<Date>();
+        }
+
+        public AddNewData(T data)
+        {
+            storedData = data;
+        }
+
+        public void PrintAllData()
+        {
+            Console.WriteLine("Data 1 berisi: " + storedData + " yang disimpan pada waktu UTC: " + inputDates);
+        }
+    }
+}
